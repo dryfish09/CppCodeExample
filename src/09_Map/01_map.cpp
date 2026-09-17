16a8f5d462f7314849685cbcc99d59359d93d210
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-09-17 17:50:05 +0700

    complete code

--- src/09_Map/01_map.cpp
+++ src/09_Map/01_map.cpp
@@ -4,8 +4,16 @@
 #include <map>
 
 int main() {
-    std::map<std::string, int> stuffInMyBag {
-        {"books", 3};
-        {"pen", 1};
-    }
+    std::map<std::string, int> countsOfStuff {
+        {"books", 3},
+        {"pen", 1},
+    };
+    // get item:
+    std::cout <<countsOfStuff.at("books");
+    // add item:
+    countsOfStuff.emplace("pencil", 2);
+    countsOfStuf.emplace("asd", 10);
+    // delete item:
+    countsOfStuff.erase("asd");
+    return 0;
 }
