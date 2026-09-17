9d84c6c78ee5a84d142a3a68176139b2d09a8d86
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-09-17 12:58:58 +0700

    fix (and also feat): fix typo and add another way to add/erase item

--- src/08_Vector/01_Vector.cpp
+++ src/08_Vector/01_Vector.cpp
@@ -7,11 +7,14 @@ int main() {
     // add item:
     numbers.push_back(4);
     numbers.push_back(5);
+    // add item from place:
+    numbers.insert(numbers.begin() + 2, 17); // add 17 to 3rd place on vector
     // remove item:
     numbers.pop_back(); // remove last item
+    numbers.erase(numbers.begin() + 3); // remove 4th item
     // access to it (like arrays!)
     std::cout << numbers[0] << std::endl;
-    // we also have safer way, to prevent Undefined Behavor (UB)
+    // we also have safer way, to prevent Undefined Behavior (UB)
     std::cout << numbers.at(1) << std::endl;
     return 0;
 }
