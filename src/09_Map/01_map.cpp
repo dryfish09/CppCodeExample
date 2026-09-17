57fd23ef43f1f50c3b1949db32189064784a6197
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-09-17 17:38:16 +0700

    Create 01_map.cpp

---
+++ src/09_Map/01_map.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+// std::map is in <map> header
+#include <map>
+
+int main() {
+    std::map<std::string, int> stuffInMyBag {
+        "books", 3,
+        "pen", 1,
+    };
+}
