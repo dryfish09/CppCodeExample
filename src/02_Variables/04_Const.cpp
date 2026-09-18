88c6a2801d70885631f80a8a57cf4208e5c9a84f
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-09-18 21:36:01 +0700

    feat: introduce `constexpr` and add `const` and `constexpr` difference at the end of file

--- src/02_Variables/04_Const.cpp
+++ src/02_Variables/04_Const.cpp
@@ -6,8 +6,26 @@ License: The Unlicense
 int main() {
     // 'const' keyword in C++ is like 'val' in Kotlin:
     const float pi = 3.14f; // can't change this. In this example, 'float' is enough
+    // constexpr - must be computable at compile-time:
+    constexpr int num1 = 2;
+    constexpr int num2 = num1 * 2 + 5; // evaluated by compiler
     // if you try to change it, compiler will be angry and throw an error.
     // print (use) it:
-    std::cout << "3 first number of pi is: " << pi << std::endl;
+    std::cout << "3 first digits of pi is: " << pi << std::endl;
     return 0;
 }
+/*
+const and constexpr difference:
+const:
+
+Meaning: value won't change 
+Initialization: runtime or compile-time
+Evaluated: whenever
+work with functions (will learn later): no
+constexpr:
+
+Meaning: Values known at compile-time
+Imitialization: Must be compile time
+Evaluated: at compiler time
+Work with functions: yes!
+*/
