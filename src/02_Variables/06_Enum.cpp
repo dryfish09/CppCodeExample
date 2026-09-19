2c977ddc2b5d1f728fd34626fa13d6baa10598e1
Dr. YFish <261328128+dryfish09@users.noreply.github.com>
2026-09-19 07:20:30 +0700

    style: rewrite enum, variables name to use lower case instead upper case
    UPPERCASE must in macro

--- src/02_Variables/06_Enum.cpp
+++ src/02_Variables/06_Enum.cpp
@@ -7,24 +7,24 @@ License: The Unlicense
 // enum is a list of variables, good for logging (logging libraries use this), etc.
 // basic example, we use scoped enum.
 enum class Color {
-    RED,
-    BLUE,
-    GREEN
+    red,
+    blue,
+    green
 };
 // enum with its value
 enum class Status : int {
-    OK = 200,
-    NOT_FOUND = 404
+    ok = 200,
+    not_found = 404
 };
 // to change enum into string, we need to write a "parser"
 std::string enumToString (Color c) {
         
     switch (c) {
-        case Color::RED:
+        case Color::red:
         return "Red"; // no need to break when we have return
-        case Color::BLUE:
+        case Color::blue:
         return "Blue";
-        case Color::GREEN:
+        case Color::green:
         return "Green";
         default:
         return "Unknown";
@@ -35,7 +35,7 @@ int main() {
     std::string myColor = enumToString(Color::RED);
     std::cout << "my color: " << myColor << std::endl;
     // type casting (to use status)
-    Status HTTPS_STATUS = Status::OK;
-    std::cout << "ok status code is: " << static_cast<int>(HTTPS_STATUS) <<std::endl;
+    Status HttpStatus = Status::ok;
+    std::cout << "ok status code is: " << static_cast<int>(HttpStatus) <<std::endl;
     return 0;
 }
